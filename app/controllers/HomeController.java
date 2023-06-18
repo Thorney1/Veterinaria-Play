22474db2ac0ece46d7d2d01f7ff52eeffa2bd518
Daniel Sepulveda <daniel-thorney@hotmail.com>
2023-06-18 03:06:13 -0400

    Implementacion de templates y enrutamiento
    Se generan los templates HTML a utilizar y se dan los enrutamientos por el método GET.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -14,11 +14,44 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-    public Result index() {
-        return ok(views.html.index.render());
-    }
+
 
     public Result login() {
         return ok(views.html.login.render());
     }
+
+    public Result home() {
+
+        return ok(views.html.home.render());
+    }
+
+    public Result homeVeterinario() {
+        return ok(views.html.homeVeterinario.render());
+    }
+
+    public Result error() {
+        return ok(views.html.error.render());
+    }
+
+    public Result mascotas() {
+        return ok(views.html.mascotas.render());
+    }
+
+    public Result atenciones() {
+        return ok(views.html.atenciones.render());
+    }
+
+    public Result atencionesAtender() {
+        return ok(views.html.atencionesAtender.render());
+    }
+
+    public Result mascotasCrear() {
+        return ok(views.html.mascotasCrear.render());
+    }
+
+    public Result register() {
+        return ok(views.html.register.render());
+    }
+
+
 }
--- build.sbt
+++ build.sbt
@@ -8,3 +8,8 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 scalaVersion := "2.13.11"
 
 libraryDependencies += guice
+libraryDependencies += "org.webjars" % "bootstrap" % "5.0.0"
+libraryDependencies += "org.webjars" % "jquery" % "3.6.0"
+libraryDependencies += "org.webjars.npm" % "sweetalert2" % "11.6.6"
+
+
