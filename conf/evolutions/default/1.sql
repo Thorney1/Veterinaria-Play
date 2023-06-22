558d5cc1d5cc4c24b46ec83d6d34e5c809a043ca
Daniel Sepulveda <daniel-thorney@hotmail.com>
2023-06-22 02:19:08 -0400

    modificaciones y experimentos

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -12,6 +12,42 @@ create table user (
   constraint pk_user primary key (id)
 );
 
+CREATE TABLE tipo_animal (
+  "id"   INTEGER,
+  "tipo" VARCHAR NOT NULL,
+  PRIMARY KEY ("id" AUTOINCREMENT)
+);
+
+CREATE TABLE estado_animal (
+"id"	                        INTEGER,
+"estado"	                    VARCHAR NOT NULL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE cliente (
+"id"	                        INTEGER,
+"name"	                        VARCHAR NOT NULL,
+"apellido"	                    VARCHAR NOT NULL,
+"cli_rut"	                    VARCHAR NOT NULL,
+"telefono"	                    VARCHAR NOT NULL,
+"direccion"	                    VARCHAR NOT NULL,
+"email"	                        VARCHAR NOT NULL UNIQUE,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE diagnostico (
+"id"	                       INTEGER,
+"animal_id"	                   ,
+"veterinaria_id"	           ,
+"descripcion"	               VARCHAR NOT NULL,
+"medicamento"	               VARCHAR NOT NULL,
+"uuid"	                       TEXT NOT NULL,
+FOREIGN KEY("veterinaria_id") REFERENCES "user"("id"),
+FOREIGN KEY("animal_id") REFERENCES "animal"("id"),
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
 
 # --- !Downs
 
